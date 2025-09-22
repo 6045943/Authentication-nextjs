8e65b2920a090abd650c268e74f537d46a147139
Kaya Altona <kaya.altona@gmail.com>
2025-09-22 12:19:34 +0200

    User auth dashboard
    heb auth gemaakt voor dat als je inlogt dat er gekeken wordt welke rol je hebt in de database en ligt eraan welke rol je hebt maar je naar een dashboard

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+import { LogoutButton } from "../../components/logout-button"
+
+export default function AdminPage() {
+  return (
+    <div>
+      <h1>Admin dashboard</h1>
+      <div>
+        <LogoutButton />
+      </div>
+    </div>
+  )
+}
+
+
